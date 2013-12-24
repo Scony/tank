1f888e4e80cba47242b3efcd4d44cf9be1e48071
Pawel Lampe <scony@sconysoft.com>
2013-12-24 10:44:20 +0100

    more scratch

---
+++ server/Game.hpp
@@ -0,0 +1,21 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+// hold list of tanks + shoot flag for each
+// update when new info arrives
+// update pos + remove tank eventually
+// bring interface toString to get actual info to send
+
+#include <list>
+
+class Game
+{
+public:
+  Game(int gid);
+  ~Game();
+
+private:
+  int gid;
+  std::list<?
+
+#endif
---
+++ server/Map.hpp
@@ -0,0 +1,17 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+// read file
+// bring interface for randing tanks positions
+// bring interface toString to send map + movables first time
+
+class Map
+{
+public:
+  Map();
+  ~Map();
+
+private:
+  // 
+
+#endif
