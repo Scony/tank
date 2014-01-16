35b4a8644b0bcdf8ed14bc58ac9be5b2fa96d601
Pawel Lampe <scony@sconysoft.com>
2014-01-16 16:34:14 +0100

    fix

--- XNA/Tanks2014/Tanks2014/Tanks2014/LobbyMode.cs
+++ XNA/Tanks2014/Tanks2014/Tanks2014/LobbyMode.cs
@@ -4,37 +4,62 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using System.Net.Sockets;
+using System.IO;
 
 namespace Tanks2014
 {
     class LobbyMode : Mode
     {
-        public LobbyMode (TanksGame host) : base(host)
-		{
-			try {
-				Int32 port = 1337;
-				string server = "127.0.0.1";
-				TcpClient client = new TcpClient (server, port);
-				NetworkStream stream = client.GetStream ();
-				Byte[] data = new Byte[256];
-				String responseData = String.Empty;
-				Int32 bytes = stream.Read (data, 0, data.Length);
-				responseData = System.Text.Encoding.ASCII.GetString (data, 0, bytes);
-				Console.WriteLine ("Received: {0}", responseData);
-				stream.Close ();
-				client.Close ();
-			} catch (ArgumentNullException e) {
-				Console.WriteLine ("ArgumentNullException: {0}", e);
-			} catch (SocketException e) {
-				Console.WriteLine ("SocketException: {0}", e);
-			} catch (Exception) {
-			}
+        private Int32 port = 1337;
+        private string server = "127.0.0.1";
+        private TcpClient client;
+        private NetworkStream stream;
+        private StreamReader sReader;
+        private string message;
+
+        public LobbyMode(TanksGame host) : base(host)
+        {
+            message = "Initializing network connection";
+            try
+            {
+                client = new TcpClient(server, port);
+                stream = client.GetStream();
+                sReader = new StreamReader(stream, Encoding.ASCII);
+                //TODO: put below close's somewhere
+                //stream.Close ();
+                //client.Close ();
+            } catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            } catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            } catch (Exception e)
+            {
+                Console.WriteLine("ExceptionA: {0}", e);
+            }
         }
+
         public override void update(GameTime gameTime)
         {
+            try
+            {
+                if (sReader.Peek() >= 0)
+                {
+                    string x = sReader.ReadLine();
+                    //TODO: analyze result and check if game is found
+                    message = x;
+                }
+            } catch (Exception)
+            {
+                message = "Could not connect to server. Reconnecting...";
+                //TODO: try reconnect
+            }
         }
+
         public override void draw(GameTime gameTime, Spriter drawer)
         {
+            Console.WriteLine("Received: " + message);
         }
     }
 }
